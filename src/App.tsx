16ec4ea1958ea7736f0e04e04c5f6fc6f546a5a6
Renan Felliphe de Moura Diogo Silva <felliphemouraa@gmail.com>
2026-02-03 22:16:02 -0300

    Adicionando validacoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import { useState } from 'react'
+import { act, useState } from 'react'
 import './App.css'
 
-export function App() {
-  const [valorDoInput, setValorDoInput] = useState<string>("")
-  const [tarefas, setTarefas] = useState<string[]>([])
+interface IErro{
+    active: boolean
+    description: string
+}
 
-  return (
-    <>
-      <div className="card">
-        <input type="text" id='input-tarefa' value={valorDoInput} onChange={(e) => setValorDoInput(e.target.value)} />
-        <button onClick={() => {
-          setTarefas(oldState => [...oldState, valorDoInput])
-          setValorDoInput("")
-        }}>Adicionar Tarefa</button>
-      </div>
-      <ul>
-        {
-          tarefas.map(tarefa => (
-            <li>{tarefa}</li>
-          ))
+export function App() {
+
+    const [valorDoInput, setValorDoInput] = useState<string>("")
+    const [tarefas, setTarefas] = useState<string[]>([])
+    const [erro, setErro] = useState<IErro>({
+        active: false,
+        description: ""
+    });
+
+    function adicionarTarefa():void{
+        setTarefas(oldState => [...oldState, valorDoInput])
+        setValorDoInput("")
+
+        if(valorDoInput.trim() === ""){            
+            setErro({
+                active: true,
+                description: "Campo obrigatório"
+            })
+
+            return;
         }
-      </ul>
-    </>
+
+        if (valorDoInput.trim().length > 15){
+            setErro({
+                active: true,
+                description: "O limite de caracteres é 15"
+            })
+
+            return;
+        }
+    }
+
+    return (
+        <>
+            <div className="card">
+                <div className="input-wrapper">
+                    <input type="text" id='input-tarefa' value={valorDoInput} onChange={(e) => setValorDoInput(e.target.value)} />
+                    <p className='erro'>{erro.active &&  erro.description}</p>
+                </div>
+                <button onClick={adicionarTarefa}>Adicionar Tarefa</button>
+            </div>
+            <ul>
+                {
+                tarefas.map(tarefa => (
+                    <li>{tarefa}</li>
+                ))
+                }
+            </ul>
+        </>
   )
 }
